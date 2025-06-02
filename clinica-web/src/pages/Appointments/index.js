2dfa752264e670002b10827a8f59eb68fd4eb9e0
maridabrins <maridabrins@hotmail.com>
2025-06-02 19:24:06 -0300

    feat: arrumando button em appointment.js

--- clinica-web/src/pages/Appointments/index.js
+++ clinica-web/src/pages/Appointments/index.js
@@ -16,8 +16,37 @@ export default function Appointments() {
 
               <div className="appointments-infos">
                 <h2>Não há consultas no momento</h2>
-            <Link to="/" className="button-primary">Agendar consulta</Link>
-              
+            <Link to="/appointments-patient" className="button-primary">Agendar consulta</Link>
+              <dialog open>
+                <div className="modal-content">
+                  <form>
+                   
+                    <label for='especialidade'>Especialidade</label>
+                    <select name="especialidade" required>
+                      <option value= 'pediatria'>Pediatria</option>
+                      <option value= 'cardiologia'>Cardiologia</option>
+                      <option value= 'dermatologia'>Dermatologia</option>
+                      <option value= 'ortopedia'>Ortopedia</option>
+                      <option value= 'neurologia'>Neurologia</option>
+                      <option value= 'otorrinolaringologista'>Otorrinolaringologista</option>
+                      <option value= 'oftamologia'>Oftamologia</option>
+
+                    </select>
+
+                    <label for='Nome'>Médico</label>
+                      <select name="Nome" required>
+                        <option value= 'medico-1' >Medico 1</option>
+                        <option value= 'medico-2'>Medico 2</option>
+                      </select>
+
+                      <label for="data">Data</label>
+                      <input name="data" type="date" required id="data-consulta"></input>
+
+                      <button className="button-primary">Agendar Consulta</button>
+                  </form>
+                </div>
+
+              </dialog>
               </div>
             
             
